af1904f8400285a2d1c7d9e67c07d95bf9528297
Potapov Denis <iljun98@users.noreply.github.com>
2018-04-19 14:31:32 +0300

    Some functions
    Potential and finding distances

--- Argon_model/Argon.cpp
+++ Argon_model/Argon.cpp
@@ -78,3 +78,54 @@ public:
 		return;
 	}
 };
+
+double find_arg(double argue, double length) {
+	double arg_1 = fmod(argue, length);
+	double arg_2;
+	if (arg_1 >= 0.) {
+		arg_2 = arg_1 - length;
+	}
+	else {
+		arg_2 = arg_1 + length;
+	}
+	if (abs(arg_1) >= abs(arg_2)) {
+		return arg_2;
+	}
+	return arg_1;
+}
+
+double count_potential(Molecule *system, int N, double length, double temperature, int time) {
+	double potential = 0;
+	double d_x, d_y, d_z;
+	double r;
+	double F;
+
+	double T0 = 300;
+
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = i + 1; j < N; j++) {
+
+			d_x = find_arg((system[j].x - system[i].x), length);
+			d_y = find_arg((system[j].y - system[i].y), length);
+			d_z = find_arg((system[j].z - system[i].z), length);
+
+			r = pow((pow(d_x, 2) + pow(d_y, 2) + pow(d_z, 2)), 0.5);
+			if (r == 0.) {
+				r = 0.000001;
+			}
+
+			potential += (4 / pow(r, 12)) - (4 / pow(r, 6)); // сделали лишнее умножение, проверка Максвелла
+
+			F = -1 * ((48 / pow(r, 14)) - (24 / pow(r, 8)));
+
+			system[i].Ax += F * d_x;
+			system[i].Ay += F * d_y;
+			system[i].Az += F * d_z;
+			system[j].Ax -= F * d_x;
+			system[j].Ay -= F * d_y;
+			system[j].Az -= F * d_z;
+
+		}
+	}
+	return potential;
+}
